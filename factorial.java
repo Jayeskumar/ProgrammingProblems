69e0c15710a4020ece02a837583f684d68dd4e88
Jayeskumar <52567903+Jayeskumar@users.noreply.github.com>
2022-06-29 15:53:58 +0000

    conit

--- factorial.java
+++ factorial.java
@@ -1,1 +1,21 @@
+class Fact{
+    //iterative factorial
+    static int fact(int n){
+        //factorial using iteration
+        int fact = 1;
+        for(int i = 1; i <= n; i++){
+            fact *= i;
+        }
+        return fact;
+    }
+    //recursive factorial
+    static int factRec(int n){
+        //factorial using recursion
+        if(n == 0){
+            return 1;
+        }
+        return n * factRec(n-1);
+    }
+    
 
+}
