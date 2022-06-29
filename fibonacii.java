33c4661bf1db8aa0a123d4ab8479dc58d25742f7
Jayeskumar <52567903+Jayeskumar@users.noreply.github.com>
2022-06-29 16:00:18 +0000

    comit

---
+++ fibonacii.java
@@ -0,0 +1,21 @@
+class Fibonacci{
+    static int fibonacci(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+    //iterative fibonacci
+    static int fibonacciIterative(int n){
+        int a=0;
+        int b=1;
+        int c=0;
+        for(intn i=0;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+
+        }
+        return c;
+
+    }
+}
