ce099acb63151bacc3652e4306996f02b71981b2
Jayeskumar <52567903+Jayeskumar@users.noreply.github.com>
2022-07-01 04:40:43 +0000

    cmt

---
+++ findsumitsum1.java
@@ -0,0 +1,8 @@
+public class findsumitsum1 {
+    /*Sumit sum has total 4 models
+     * 1. Smallest digits from two digit numbers from 5 nu
+     * 2. Smallest digits from three digit numbers
+     * 3. G
+     */
+    
+}
